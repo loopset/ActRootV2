6c6bf8b614087ec40aaf8f8ea1bf6c0dc315ad84
loopset <miguellozanogonzalez119@gmail.com>
2023-10-09 18:06:17 +0200

    added S2n and S2p funcs

--- Physics/ActParticle.h
+++ Physics/ActParticle.h
@@ -32,7 +32,9 @@ namespace ActPhysics
         double GetMass() const {return fMass;}//!< Return mass in MeV / c2 units
         double GetAMU() const {return fMass / Constants::kamuToMeVC2;}//!< Return mass in amu units
         double GetSn() const {return GetSnX(1);}
+        double GetS2n() const {return GetSnX(2);}
         double GetSp() const {return GetSpX(1);}
+        double GetS2p() const {return GetSpX(2);}
         void Print() const;
 
     private:
