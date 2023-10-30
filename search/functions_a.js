0531a411468938a937a5745a932eb204430a7f1c
loopset <miguellozanogonzalez119@gmail.com>
2023-10-30 11:31:51 +0000

    Deploying to gh-pages from @ loopset/ActRootV2@3b08a5f58c8806187d9f45c67a3bc9d76c3e42bb 🚀

--- search/functions_a.js
+++ search/functions_a.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['length_1404',['Length',['../classActCluster_1_1Interval.html#a3a1aeec52b6a4464ce9cb86efa377209',1,'ActCluster::Interval']]],
-  ['line_1405',['Line',['../classActPhysics_1_1Line.html#aa1a46ad70bee19aeb2d7d1685a820be1',1,'ActPhysics::Line::Line()=default'],['../classActPhysics_1_1Line.html#a4a178deb2cd606f5cced01cbcc2d7b21',1,'ActPhysics::Line::Line(XYZPoint point, XYZVector direction, float chi)'],['../classActPhysics_1_1Line.html#abaab8509c2ae394e227777d1ee487165',1,'ActPhysics::Line::Line(const XYZPoint &amp;p1, const XYZPoint &amp;p2)']]]
+  ['line_1405',['Line',['../classActPhysics_1_1Line.html#aa1a46ad70bee19aeb2d7d1685a820be1',1,'ActPhysics::Line::Line()=default'],['../classActPhysics_1_1Line.html#acc0ffb8107c9edd1066431d3c94a9d43',1,'ActPhysics::Line::Line(XYZPoint point, XYZVector direction, double chi)'],['../classActPhysics_1_1Line.html#abaab8509c2ae394e227777d1ee487165',1,'ActPhysics::Line::Line(const XYZPoint &amp;p1, const XYZPoint &amp;p2)']]]
 ];
