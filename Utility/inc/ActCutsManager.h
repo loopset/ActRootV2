0ce9b9cb2719c6250d2d55f3225a0e5bc4944b3e
loopset <miguellozanogonzalez119@gmail.com>
2024-03-26 17:13:09 +0100

    minor bugfix in cutsmanager
    just edited an error message

--- Utility/inc/ActCutsManager.h
+++ Utility/inc/ActCutsManager.h
@@ -17,106 +17,106 @@
 
 namespace ActRoot
 {
-    template <typename T>
-    class CutsManager
-    {
-    private:
-        std::unordered_map<T, TCutG*> fCuts {};
+template <typename T>
+class CutsManager
+{
+private:
+    std::unordered_map<T, TCutG*> fCuts {};
 
-    public:
-        bool ReadCut(const T& key, std::string_view file, std::string_view name = "CUTG");
-        void DrawCut(const T& key);
-        void DrawAll();
-        bool IsInside(const T& key, double x, double y);
-        std::optional<T> GetKeyIsInside(double x, double y);
-        TCutG* GetCut(const T& key);
-        void SetLineAttributes(const T& key, Color_t color, Width_t width = 1);
-        std::vector<T> GetListOfKeys() const;
-    };
+public:
+    bool ReadCut(const T& key, std::string_view file, std::string_view name = "CUTG");
+    void DrawCut(const T& key);
+    void DrawAll();
+    bool IsInside(const T& key, double x, double y);
+    std::optional<T> GetKeyIsInside(double x, double y);
+    TCutG* GetCut(const T& key);
+    void SetLineAttributes(const T& key, Color_t color, Width_t width = 1);
+    std::vector<T> GetListOfKeys() const;
+};
 
-    template <typename T>
-    inline bool CutsManager<T>::ReadCut(const T& key, std::string_view file, std::string_view name)
+template <typename T>
+inline bool CutsManager<T>::ReadCut(const T& key, std::string_view file, std::string_view name)
+{
+    auto* infile {new TFile(file.data())};
+    if(infile->IsZombie())
     {
-        auto* infile {new TFile(file.data())};
-        if(infile->IsZombie())
-        {
-            std::cout << "CutsManager::ReadCut(): file " << file << " doesn't exist!" << '\n';
-            fCuts[key] = nullptr;
-            return false;
-        }
-        auto* cut {infile->Get<TCutG>(name.data())};
-        if(!cut)
-            throw std::runtime_error("CutsManager::ReadCut(): file " + std::string(name) +
-                                     " doesn't contain a valid TCutG");
-        fCuts[key] = cut;
-        delete infile;
-        return true;
+        std::cout << "CutsManager::ReadCut(): file " << file << " doesn't exist!" << '\n';
+        fCuts[key] = nullptr;
+        return false;
     }
+    auto* cut {infile->Get<TCutG>(name.data())};
+    if(!cut)
+        throw std::runtime_error("CutsManager::ReadCut(): file " + std::string(file) +
+                                 " doesn't contain a TCutG named " + std::string(name));
+    fCuts[key] = cut;
+    delete infile;
+    return true;
+}
 
-    template <typename T>
-    inline void CutsManager<T>::DrawCut(const T& key)
+template <typename T>
+inline void CutsManager<T>::DrawCut(const T& key)
+{
+    if(fCuts[key])
     {
-        if(fCuts[key])
-        {
-            fCuts[key]->SetLineColor(kRed);
-            fCuts[key]->Draw("same");
-        }
+        fCuts[key]->SetLineColor(kRed);
+        fCuts[key]->Draw("same");
     }
+}
 
-    template <typename T>
-    inline void CutsManager<T>::DrawAll()
+template <typename T>
+inline void CutsManager<T>::DrawAll()
+{
+    for(auto& [key, cut] : fCuts)
     {
-        for(auto& [key, cut] : fCuts)
-        {
-            if(cut)
-                cut->Draw("same");
-        }
+        if(cut)
+            cut->Draw("same");
     }
+}
 
-    template <typename T>
-    inline bool CutsManager<T>::IsInside(const T& key, double x, double y)
-    {
-        if(fCuts[key])
-            return fCuts[key]->IsInside(x, y);
-        else
-            return false;
-    }
+template <typename T>
+inline bool CutsManager<T>::IsInside(const T& key, double x, double y)
+{
+    if(fCuts[key])
+        return fCuts[key]->IsInside(x, y);
+    else
+        return false;
+}
 
-    template <typename T>
-    inline std::optional<T> CutsManager<T>::GetKeyIsInside(double x, double y)
+template <typename T>
+inline std::optional<T> CutsManager<T>::GetKeyIsInside(double x, double y)
+{
+    for(const auto& [key, cut] : fCuts)
     {
-        for(const auto& [key, cut] : fCuts)
-        {
-            if(cut)
-                if(cut->IsInside(x, y))
-                    return std::optional<T>(key);
-        }
-        return std::nullopt;
+        if(cut)
+            if(cut->IsInside(x, y))
+                return std::optional<T>(key);
     }
+    return std::nullopt;
+}
 
-    template <typename T>
-    inline TCutG* CutsManager<T>::GetCut(const T& key)
-    {
-        return fCuts[key];
-    }
+template <typename T>
+inline TCutG* CutsManager<T>::GetCut(const T& key)
+{
+    return fCuts[key];
+}
 
-    template <typename T>
-    inline void CutsManager<T>::SetLineAttributes(const T& key, Color_t color, Width_t width)
+template <typename T>
+inline void CutsManager<T>::SetLineAttributes(const T& key, Color_t color, Width_t width)
+{
+    if(fCuts[key])
     {
-        if(fCuts[key])
-        {
-            fCuts[key]->SetLineColor(color);
-            fCuts[key]->SetLineWidth(width);
-        }
+        fCuts[key]->SetLineColor(color);
+        fCuts[key]->SetLineWidth(width);
     }
+}
 
-    template <typename T>
-    inline std::vector<T> CutsManager<T>::GetListOfKeys() const
-    {
-        std::vector<T> ret;
-        for(const auto& [key, _] : fCuts)
-            ret.push_back(key);
-        return ret;
-    }
+template <typename T>
+inline std::vector<T> CutsManager<T>::GetListOfKeys() const
+{
+    std::vector<T> ret;
+    for(const auto& [key, _] : fCuts)
+        ret.push_back(key);
+    return ret;
+}
 } // namespace ActRoot
 #endif
