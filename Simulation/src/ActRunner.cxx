f79e72dfb34dcfc1866b5a31827000e29eda62bf
loopset <miguellozanogonzalez119@gmail.com>
2024-05-19 19:02:26 +0200

    major bug corrected in Runner::EnergyAfterSilicons
    energy loss was being referred to initial energy...
    
    checked that the issue only happenned in this function

--- Simulation/src/ActRunner.cxx
+++ Simulation/src/ActRunner.cxx
@@ -1,51 +1,44 @@
 #include "ActRunner.h"
 
-#include "ActSRIM.h"
 #include "ActGeometry.h"
+#include "ActSRIM.h"
 
 #include "TH3.h"
 #include "TMath.h"
 #include "TRandom3.h"
 
 #include <cmath>
-#include <iostream>
-#include <stdexcept>
 #include <string>
 #include <utility>
 
-ActSim::Runner::Runner(ActPhysics::SRIM* sri,
-                       Geometry* geo,
-                       TRandom3* rand,
-                       double silSigma)
-    : fsrim(sri), fgeo(geo), fRand(rand)
+ActSim::Runner::Runner(ActPhysics::SRIM* sri, Geometry* geo, TRandom3* rand, double silSigma)
+    : fsrim(sri),
+      fgeo(geo),
+      fRand(rand)
 {
-    //initialize sil resolution with sigma at 5.5 MeV
-    fSilResolution = std::make_unique<TF1>("fSilResolution",
-                                           [=](double* x, double* p)
-                                           {return silSigma * TMath::Sqrt(x[0] / 5.5);},
-                                           0.0, 100.0, 1);
+    // initialize sil resolution with sigma at 5.5 MeV
+    fSilResolution = std::make_unique<TF1>(
+        "fSilResolution", [=](double* x, double* p) { return silSigma * TMath::Sqrt(x[0] / 5.5); }, 0.0, 100.0, 1);
 }
 
-std::pair<double, double> ActSim::Runner::EnergyAfterSilicons(double T3EnteringSil,
-                                                             double silWidth,
-                                                             double silThresh,
-                                                             const std::string& silString,
-                                                             bool enableResolution,
-                                                             bool enableStraggling)
+std::pair<double, double> ActSim::Runner::EnergyAfterSilicons(double T3EnteringSil, double silWidth, double silThresh,
+                                                              const std::string& silString, bool enableResolution,
+                                                              bool enableStraggling)
 {
-    //defs
-    double eLoss {}; double T3AfterSil0 {};
-    //initial range
+    // defs
+    double eLoss {};
+    double T3AfterSil0 {};
+    // initial range
     auto RIni {fsrim->EvalDirect(silString, T3EnteringSil)};
-    //RLeft without straggling
+    // RLeft without straggling
     auto RLeft {RIni - silWidth};
-    if(RLeft < 0)//particle stopped in silicon
+    if(RLeft < 0) // particle stopped in silicon
     {
         eLoss = T3EnteringSil;
         if(enableResolution)
             eLoss = fRand->Gaus(T3EnteringSil, fSilResolution->Eval(T3EnteringSil));
         T3AfterSil0 = 0.;
-        //if threshold
+        // if threshold
         if(eLoss < silThresh)
             return {std::nan("threshold"), 0.};
         else
@@ -59,9 +52,9 @@ std::pair<double, double> ActSim::Runner::EnergyAfterSilicons(double T3EnteringS
         if(enableStraggling)
             RLeftWithStraggling = ApplyStragglingInMaterialToRLeft(RIni, RLeft, silString);
         else
-            RLeftWithStraggling = RLeft;//no straggling
-        
-        T3AfterSil0 = T3EnteringSil - fsrim->EvalInverse(silString, RLeftWithStraggling);
+            RLeftWithStraggling = RLeft; // no straggling
+
+        T3AfterSil0 = fsrim->EvalInverse(silString, RLeftWithStraggling);
 
         eLoss = {T3EnteringSil - T3AfterSil0};
         if(enableResolution)
@@ -70,29 +63,26 @@ std::pair<double, double> ActSim::Runner::EnergyAfterSilicons(double T3EnteringS
         if(eLoss < silThresh)
             return {std::nan("threshold"), T3AfterSil0};
         else
-            return {eLoss, T3AfterSil0};    
+            return {eLoss, T3AfterSil0};
     }
 }
 
-double ActSim::Runner::ApplyStragglingInMaterialToRLeft(double RIni,
-                                                       double RLeft,
-                                                       const std::string& srimKey)
+double ActSim::Runner::ApplyStragglingInMaterialToRLeft(double RIni, double RLeft, const std::string& srimKey)
 {
     auto distance {RIni - RLeft};
     auto straggRIni {fsrim->EvalLongStraggling(srimKey, RIni)};
-    auto straggRLeft{fsrim->EvalLongStraggling(srimKey, RLeft)};
+    auto straggRLeft {fsrim->EvalLongStraggling(srimKey, RLeft)};
     auto straggDist {TMath::Sqrt(TMath::Power(straggRIni, 2) - TMath::Power(straggRLeft, 2))};
     double RLeftWithStraggling {};
     do
     {
         auto distanceStragg {fRand->Gaus(distance, straggDist)};
         RLeftWithStraggling = RIni - distanceStragg;
-    }
-    while(RLeftWithStraggling < 0.);
-    return RLeftWithStraggling;//return RLeft after straggling
+    } while(RLeftWithStraggling < 0.);
+    return RLeftWithStraggling; // return RLeft after straggling
 }
 
-double ActSim::Runner::EnergyAfterGas(double TIni, double distance, const std::string &gasKey, bool straggling)
+double ActSim::Runner::EnergyAfterGas(double TIni, double distance, const std::string& gasKey, bool straggling)
 {
     auto RIni {fsrim->EvalDirect(gasKey, TIni)};
     auto RLeft {RIni - distance};
@@ -107,29 +97,29 @@ double ActSim::Runner::EnergyAfterGas(double TIni, double distance, const std::s
     }
 }
 
-double ActSim::Runner::EnergyBeforeGas(double Esil, double trackLengthInMM,
-                                      const std::string& gasKey)
+double ActSim::Runner::EnergyBeforeGas(double Esil, double trackLengthInMM, const std::string& gasKey)
 {
     auto RIni {fsrim->EvalDirect(gasKey, Esil)};
     auto RVertex {RIni + trackLengthInMM};
     return fsrim->EvalInverse(gasKey, RVertex);
 }
 
-ActSim::Runner::XYZPoint ActSim::Runner::SampleVertex(double meanY, double sigmaY, double meanZ, double sigmaZ, TH3F* histBeam)
+ActSim::Runner::XYZPoint
+ActSim::Runner::SampleVertex(double meanY, double sigmaY, double meanZ, double sigmaZ, TH3F* histBeam)
 {
     XYZPoint ret {};
-    double x { fRand->Uniform() * fgeo->GetDriftParameters().X * 2 *10.};//*2 (half length to length) and * 10. (cm to mm)
-    double y {}; double z {};
+    double x {fRand->Uniform() * fgeo->GetDriftParameters().X * 2 *
+              10.}; //*2 (half length to length) and * 10. (cm to mm)
+    double y {};
+    double z {};
     if(histBeam)
     {
         z = fRand->Gaus(meanZ, sigmaZ);
-        double thetaXYHist {}; double thetaXZHist {};
+        double thetaXYHist {};
+        double thetaXZHist {};
         histBeam->GetRandom3(y, thetaXYHist, thetaXZHist);
-        ret = {
-            x,
-            y - x * TMath::Tan(thetaXYHist * TMath::DegToRad()),
-            z - x * TMath::Tan(thetaXZHist * TMath::DegToRad())
-        };
+        ret = {x, y - x * TMath::Tan(thetaXYHist * TMath::DegToRad()),
+               z - x * TMath::Tan(thetaXZHist * TMath::DegToRad())};
     }
     else
     {
@@ -140,25 +130,18 @@ ActSim::Runner::XYZPoint ActSim::Runner::SampleVertex(double meanY, double sigma
     return ret;
 }
 
-ActSim::Runner::XYZPoint ActSim::Runner::DisplacePointToTGeometryFrame(const XYZPoint &point)
+ActSim::Runner::XYZPoint ActSim::Runner::DisplacePointToTGeometryFrame(const XYZPoint& point)
 {
-    XYZPoint ret
-        {
-            point.X() / 10. - fgeo->GetDriftParameters().X,
-            point.Y() / 10. - fgeo->GetDriftParameters().Y,
-            point.Z() / 10. - fgeo->GetDriftParameters().Z
-        };
+    XYZPoint ret {point.X() / 10. - fgeo->GetDriftParameters().X, point.Y() / 10. - fgeo->GetDriftParameters().Y,
+                  point.Z() / 10. - fgeo->GetDriftParameters().Z};
     return ret;
 }
 
-ActSim::Runner::XYZPoint ActSim::Runner::DisplacePointToStandardFrame(const XYZPoint &pointGeo)
+ActSim::Runner::XYZPoint ActSim::Runner::DisplacePointToStandardFrame(const XYZPoint& pointGeo)
 {
-    XYZPoint ret
-        {
-            (pointGeo.X() + fgeo->GetDriftParameters().X) * 10.,
-            (pointGeo.Y() + fgeo->GetDriftParameters().Y) * 10. ,
-            (pointGeo.Z() + fgeo->GetDriftParameters().Z) * 10.
-        };
+    XYZPoint ret {(pointGeo.X() + fgeo->GetDriftParameters().X) * 10.,
+                  (pointGeo.Y() + fgeo->GetDriftParameters().Y) * 10.,
+                  (pointGeo.Z() + fgeo->GetDriftParameters().Z) * 10.};
     return ret;
 }
 
