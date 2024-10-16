3e1784d5f5372afddd3933c87fe478a300239977
ibc12 <ivan.blanco.ordes@gmail.com>
2024-10-16 14:27:44 +0200

    add changes to FindRP Action

--- Algorithm/Actions/ActAFindRP.h
+++ Algorithm/Actions/ActAFindRP.h
@@ -11,6 +11,7 @@ class FindRP : public VAction
 private:
     double fBeamLikeXMinThresh {}; // Min value in X that has to be in the cluster to be beam-like
     double fBeamLikeParallelF {};  // Min value for X component
+    double fRPDistThresh {};       // Max distance between two lines to form a RP
 public:
     FindRP() : VAction("FindRP") {}
 
@@ -18,6 +19,7 @@ public:
     void Run() override;
     void Print() const override;
     void DetermineBeamLikes();
+    void FindPreliminaryRP();
 };
 } // namespace Actions
 } // namespace ActAlgorithm
