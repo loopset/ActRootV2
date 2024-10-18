7602aea0c1ff6516758c411dfa3be447193c285f
ibc12 <ivan.blanco.ordes@gmail.com>
2024-10-18 16:37:47 +0200

    test git again again new pc

--- Algorithm/Actions/ActAFindRP.cxx
+++ Algorithm/Actions/ActAFindRP.cxx
@@ -111,7 +111,7 @@ void ActAlgorithm::Actions::FindRP::FindPreliminaryRP()
             //    rps.push_back({rp, {i, j}});
             //else
             //    continue;
-            //test again
+            //test again again
         }
     }
 }
