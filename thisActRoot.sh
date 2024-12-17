c735599638e341eecaa89b46a03a93a4012ef750
loopset <miguellozanogonzalez119@gmail.com>
2024-12-17 18:04:06 +0100

    fixed thisactroot.sh

--- thisActRoot.sh
+++ thisActRoot.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 
 # Self locate script when sourced
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+if [ -n "$BASH_VERSION" ]; then
+    SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+elif [ -n "$ZSH_VERSION" ]; then
+    SCRIPT_DIR=$( cd -- "$( dirname -- "${0}" )" &> /dev/null && pwd )
+else
+    echo "thisActRoot.sh: unsupported shell to source"
+    exit 1
+fi
 
 # Add to system libraries
 lib="${SCRIPT_DIR}/install/lib"
