4edee665a23189867869fe3a2956202fd647b11a
loopset <miguellozanogonzalez119@gmail.com>
2025-01-08 17:10:22 +0000

    Deploying to gh-pages from @ loopset/ActRootV2@cdb060ffa2f729dafebea4bafdb2c515eea1d36a 🚀

--- search/classes_3.js
+++ search/classes_3.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['datamanager_0',['DataManager',['../classActRoot_1_1DataManager.html',1,'ActRoot']]],
-  ['detectormanager_1',['DetectorManager',['../classActRoot_1_1DetectorManager.html',1,'ActRoot']]],
-  ['driftchamber_2',['DriftChamber',['../structActSim_1_1DriftChamber.html',1,'ActSim']]]
+  ['decaygenerator_1',['DecayGenerator',['../classActSim_1_1DecayGenerator.html',1,'ActSim']]],
+  ['detectormanager_2',['DetectorManager',['../classActRoot_1_1DetectorManager.html',1,'ActRoot']]],
+  ['driftchamber_3',['DriftChamber',['../structActSim_1_1DriftChamber.html',1,'ActSim']]]
 ];
