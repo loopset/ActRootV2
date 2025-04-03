40c20e20d013f8ebf2552ef86b54fff11d413dcb
ibc12 <ivan.blanco.ordes@gmail.com>
2025-04-03 12:03:53 +0200

    Add implementation to ActSplit (RANSAC)

--- Algorithm/Actions/ActASplit.h
+++ Algorithm/Actions/ActASplit.h
@@ -16,6 +16,10 @@ namespace Actions
 class Split : public VAction
 {
 private:
+    int fNiterRANSAC {};       // Iterations for RANSAC algorithm
+    double fCylinderRadius {}; // Cylinder radius to define inliers
+    double fMinChi2 {};        // Minimum value of cluster's chi2 to apply RANSAC to it
+    int fSavedIterations {};   // Iterations with higher amount of inliers used to get best cluster (least chi2)
 public:
     Split() : VAction("Split") {}
 
@@ -24,6 +28,18 @@ public:
     void Print() const override {};
 
 private:
+    void CheckChi2(std::vector<ActRoot::Cluster>& clusters, std::vector<ActRoot::Cluster>& clustersToRANSAC,
+                   std::vector<ActRoot::Cluster>& clustersFinal);
+    std::pair<std::vector<ActRoot::Cluster>, std::vector<ActRoot::Cluster>>
+    SortClustersInliersAndOutliers(std::vector<ActRoot::Cluster>& inliersVector,
+                                   std::vector<ActRoot::Cluster>& outliersVector);
+    std::pair<std::vector<ActRoot::Cluster>, std::vector<ActRoot::Cluster>>
+    GetInliersAndOutliers(ActRoot::Cluster& cluster, int Niterations);
+    std::pair<ActRoot::Cluster&, ActRoot::Cluster&>
+    GetBestFit(std::pair<std::vector<ActRoot::Cluster>, std::vector<ActRoot::Cluster>>& inliersAndOutliersVector,
+               int nClusterFit);
+    void ApplyContinuity(std::vector<ActRoot::Cluster>& clustersIteration, ActRoot::Cluster& outliers,
+                         ActRoot::Cluster& bestCluster, ActAlgorithm::ClIMB* climb);
 };
 } // namespace Actions
 } // namespace ActAlgorithm
