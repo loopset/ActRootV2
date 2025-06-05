95c59e0a35700e341959014e475d81b268925bb8
loopset <miguellozanogonzalez119@gmail.com>
2025-06-05 08:17:05 +0000

    Deploying to gh-pages from @ loopset/ActRootV2@6f39a57a32e640b264043690bd182a81a0c9a851 🚀

--- search/functions_16.js
+++ search/functions_16.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['x_0',['X',['../classActRoot_1_1TPCParameters.html#a7cdc3d6581c85e385f14f9cf1285d881',1,'ActRoot::TPCParameters']]],
-  ['xsintervalcm_1',['xsIntervalcm',['../classActSim_1_1CrossSection.html#a1621d28647ece0a83a283a2cbdbe45cc',1,'ActSim::CrossSection']]]
+  ['x_0',['X',['../classActRoot_1_1TPCParameters.html#a7cdc3d6581c85e385f14f9cf1285d881',1,'ActRoot::TPCParameters']]]
 ];
